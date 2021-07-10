bb06717d450a5d3697247ce8a3f94a6e96fba7f0
David Marsal Espuny <marsaldavid@hotmail.com>
2021-07-10 19:38:20 +0200

    sync

--- config/database.php
+++ config/database.php
@@ -35,13 +35,18 @@ return [
 
     'connections' => [
 
+        // 'sqlite' => [
+        //     'driver' => 'sqlite',
+        //     'url' => env('DATABASE_URL'),
+        //     'database' => env('DB_DATABASE', database_path('database.sqlite')),
+        //     'prefix' => '',
+        //     'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        // ],
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
+            'database' => dirname(__DIR__).'/database/database.sqlite',
+          ],
+
 
         'mysql' => [
             'driver' => 'mysql',
